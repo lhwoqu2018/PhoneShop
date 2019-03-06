6fc151618d11eb60ad9e4e8f3e5042e7d3e4ef04
Liu Hao <524825901@qq.com>
2019-03-06 10:44:14 +0800

    first commit

---
+++ Features/Authentication/TokenViewModel.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace ECommerce.Features.Authentication
+{
+    public class TokenViewModel
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+        [JsonProperty("access_token_expiration")]
+        public DateTime AccessTokenExpiration { get; set; }
+
+        //public string firstName { get; set; }
+        //public string lastName { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
